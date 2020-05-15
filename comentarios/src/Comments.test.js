096e6f5bf62830cdb746f2f8e74b62177f88fca2
Yago Ananias <yago_souza92@yahoo.com.br>
2020-05-15 16:35:26 -0300

    Testando com Shallow Rendering

---
+++ comentarios/src/Comments.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Comments from './Comments'
+import Comment from './Comment'
+
+describe('<Comments />', () => {
+  test('should render Comments', () => {
+    const comments = [
+      { id: 'a', coment: 'Comment 1'},
+      { id: 'b', coment: 'Comment 2'}
+    ]
+    const wrapper = shallow(<Comments comments={comments} />)
+    expect(wrapper.find(Comment).length).toBe(2)
+  })
+})
